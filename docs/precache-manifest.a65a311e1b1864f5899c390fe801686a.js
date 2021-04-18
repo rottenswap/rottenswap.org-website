431dbc1a4ab5bec041940012dc3cb1ee9f27295e
thetimtempleton <thetimtempleton@github.com>
2021-04-18 16:56:41 +0000

    init

---
+++ docs/precache-manifest.a65a311e1b1864f5899c390fe801686a.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd73779df21d5bbd67928e835671e103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2702a9cb09a0f369651",
+    "url": "/static/css/main.81261090.chunk.css"
+  },
+  {
+    "revision": "07925f172b596bca7d11",
+    "url": "/static/js/0.0a02ae70.chunk.js"
+  },
+  {
+    "revision": "0de508c638d9abdd70a2",
+    "url": "/static/js/10.42422513.chunk.js"
+  },
+  {
+    "revision": "daa18eae5f3efd176175",
+    "url": "/static/js/3.1c99398f.chunk.js"
+  },
+  {
+    "revision": "d923902410cc773b229121ac0f4349fe",
+    "url": "/static/js/3.1c99398f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17a47678b52d0c7972f",
+    "url": "/static/js/4.8eca5d6f.chunk.js"
+  },
+  {
+    "revision": "a279dc6a2fec84be23fe",
+    "url": "/static/js/5.ecf7ceb9.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/5.ecf7ceb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15328d5c06412d1d3eb",
+    "url": "/static/js/6.9b00a311.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.9b00a311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a557a03baa5ee9864f32",
+    "url": "/static/js/7.c200b1a6.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.c200b1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfaa58f3246dc09ce18c",
+    "url": "/static/js/8.a0c5e481.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.a0c5e481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438efbf70b1596f220c3",
+    "url": "/static/js/9.9e3a1ac2.chunk.js"
+  },
+  {
+    "revision": "c2702a9cb09a0f369651",
+    "url": "/static/js/main.fabbbb30.chunk.js"
+  },
+  {
+    "revision": "a57240bb42f8e3ec1909",
+    "url": "/static/js/runtime-main.03553606.js"
+  },
+  {
+    "revision": "d1b817574b80310ce045e4b5de34d323",
+    "url": "/static/media/bloody.d1b81757.png"
+  },
+  {
+    "revision": "f0df40561cc4ea9a1fc970a613423b58",
+    "url": "/static/media/chef.f0df4056.png"
+  },
+  {
+    "revision": "eba13ae89c28aa60a8401585445221b1",
+    "url": "/static/media/comp.eba13ae8.png"
+  },
+  {
+    "revision": "9c90fd037c96a856306b3bd35f71f368",
+    "url": "/static/media/crv.9c90fd03.png"
+  },
+  {
+    "revision": "0c9224c0496005dabfe11130ff4a200b",
+    "url": "/static/media/dai.0c9224c0.png"
+  },
+  {
+    "revision": "5dcb5ef380f6f557a91f750f7393dbf5",
+    "url": "/static/media/eth.5dcb5ef3.png"
+  },
+  {
+    "revision": "f1eb09c6d5371096efbfe969d9fcf44b",
+    "url": "/static/media/jpow.f1eb09c6.png"
+  },
+  {
+    "revision": "cc1d1c86077c581d882ec65f3354080c",
+    "url": "/static/media/lend.cc1d1c86.png"
+  },
+  {
+    "revision": "d52cda8cbc9d72040067a2c2a1024754",
+    "url": "/static/media/link.d52cda8c.png"
+  },
+  {
+    "revision": "2ae21a4d6aaa852983a09ea071ab3af1",
+    "url": "/static/media/maggot.2ae21a4d.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "520a37803060a508d384607c88879cef",
+    "url": "/static/media/nice.520a3780.png"
+  },
+  {
+    "revision": "e7bd9fe5049d1951caa09b352f6f6e5f",
+    "url": "/static/media/ren.e7bd9fe5.png"
+  },
+  {
+    "revision": "4887db9084fcb990879dda0bc850aa0f",
+    "url": "/static/media/rot-sushi.4887db90.png"
+  },
+  {
+    "revision": "bb8f50ab150f3fcb20aa651b1890c269",
+    "url": "/static/media/rot-uni.bb8f50ab.png"
+  },
+  {
+    "revision": "5a0b5d19bf8c74d0c2dfda527082d32e",
+    "url": "/static/media/rot.5a0b5d19.png"
+  },
+  {
+    "revision": "24ddab5e484677987db2653ce3a15edc",
+    "url": "/static/media/snx.24ddab5e.png"
+  },
+  {
+    "revision": "679aed55b98690cb4d5be27ee012d21b",
+    "url": "/static/media/srm.679aed55.png"
+  },
+  {
+    "revision": "b4ea068d73ce0e9b8b4e9f4fbbe68c2c",
+    "url": "/static/media/uni.b4ea068d.png"
+  },
+  {
+    "revision": "5314aa345ff06b33939c8d3944864455",
+    "url": "/static/media/usdc.5314aa34.png"
+  },
+  {
+    "revision": "a7c8e2f7985e59eff7f245458cf95cce",
+    "url": "/static/media/usdt.a7c8e2f7.png"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  },
+  {
+    "revision": "6b5d469ae50feaeaed00a3abcffdd5bf",
+    "url": "/static/media/wbtc.6b5d469a.png"
+  },
+  {
+    "revision": "64375ef93ce50f7dc2fbfce49a1338a2",
+    "url": "/static/media/yfi.64375ef9.png"
+  }
+]);
